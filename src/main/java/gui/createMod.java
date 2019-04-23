6e2f685a2afc42cce5d7e942412a882f04003da8
Ketkev <kdjoosten@gmail.com>
2019-04-23 23:37:25 +0200

    added new mod creating interface

--- src/main/java/gui/createMod.java
+++ src/main/java/gui/createMod.java
@@ -5,60 +5,86 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
+import net.miginfocom.swing.MigLayout;
 
 public class createMod extends JDialog implements ActionListener {
-    JLabel jlOriginal, jlModded;
-    JButton jbOriginal, jbModded, jbCreate;
-    File fOriginal, fModded;
 
-    public createMod(JFrame frame) {
-        super(frame, true);
-        setTitle("Create mod");
-        setSize(320, 140);
-        setLayout(new FlowLayout());
+  String username, description, modName;
+  JLabel jlOriginal, jlModded, jlUsername, jlDescription, jlModName;
+  JTextField jtfUsername, jtfDescription, jtfModName;
+  JButton jbOriginal, jbModded, jbCreate;
+  File fOriginal, fModded;
 
-        jlOriginal = new JLabel("Select the original dll: ");
-        jbOriginal = new JButton("Select file...");
-        jbOriginal.addActionListener(this);
-        add(jlOriginal);
-        add(jbOriginal);
+  public createMod(JFrame frame) {
+    super(frame, true);
+    setTitle("Create mod");
+    setSize(500, 140);
+    setLayout(new MigLayout());
 
-        jlModded = new JLabel("Select the modded dll: ");
-        jbModded = new JButton("Select file...");
-        jbModded.addActionListener(this);
-        add(jlModded);
-        add(jbModded);
+    jlOriginal = new JLabel("Select the original dll: ");
+    jbOriginal = new JButton("Select file...");
+    ToolTipManager.sharedInstance().setInitialDelay(0);
+    jbOriginal.setToolTipText("Make sure the original dll still has it's original name");
+    jbOriginal.addActionListener(this);
+    add(jlOriginal, "cell 0 0");
+    add(jbOriginal, "cell 1 0");
 
-        jbCreate = new JButton("Create mod");
-        jbCreate.addActionListener(this);
-        add(jbCreate);
+    jlModded = new JLabel("Select the modded dll: ");
+    jbModded = new JButton("Select file...");
+    jbModded.addActionListener(this);
+    add(jlModded, "cell 0 1");
+    add(jbModded, "cell 1 1");
 
-        setVisible(true);
+    jbCreate = new JButton("Create mod");
+    jbCreate.addActionListener(this);
+    add(jbCreate, "cell 1 2");
+
+    jlModName = new JLabel("Enter the mod's name: ");
+    jtfModName = new JTextField(10);
+    add(jlModName, "cell 2 0");
+    add(jtfModName, "cell 3 0");
+
+    jlUsername = new JLabel("Your username: ");
+    jtfUsername = new JTextField(10);
+    add(jlUsername, "cell 2 1");
+    add(jtfUsername, "cell 3 1");
+
+    jlDescription = new JLabel("Enter a description:");
+    jtfDescription = new JTextField(10);
+    add(jlDescription, "cell 2 2");
+    add(jtfDescription, "cell 3 2");
+
+    setVisible(true);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == jbOriginal) {
+      filechooser fcOriginal = new filechooser(this);
+      fOriginal = fcOriginal.getFile();
+      if (fOriginal != null) {
+        jbOriginal.setText(fOriginal.getName());
+      }
     }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == jbOriginal) {
-            filechooser fcOriginal = new filechooser(this);
-            fOriginal = fcOriginal.getFile();
-            if(fOriginal != null) {
-                jbOriginal.setText(fOriginal.getName());
-            }
-        }
-        if(e.getSource() == jbModded) {
-            filechooser fcModded = new filechooser(this);
-            fModded = fcModded.getFile();
-            if(fModded != null) {
-                jbModded.setText(fModded.getName());
-            }
-        }
-        if(e.getSource() == jbCreate) {
-            if(fModded != null && fOriginal != null) {
-                System.out.println("creating mod");
-                Mod.generateMod(fOriginal, fModded);
-                System.out.println("Created mod");
-                setVisible(false);
-            }
-        }
+    if (e.getSource() == jbModded) {
+      filechooser fcModded = new filechooser(this);
+      fModded = fcModded.getFile();
+      if (fModded != null) {
+        jbModded.setText(fModded.getName());
+      }
     }
+    if (e.getSource() == jbCreate) {
+      if (fModded != null && fOriginal != null && !jtfModName.getText().equals("") && !jtfDescription
+          .getText().equals("") && !jtfUsername.getText()
+          .equals("")) { //TODO this can be done better
+        username = jtfUsername.getText();
+        modName = jtfModName.getText();
+        description = jtfDescription.getText();
+        System.out.println("creating mod");
+        Mod.generateMod(fOriginal, fModded, username, description, modName);
+        System.out.println("Created mod");
+        setVisible(false);
+      }
+    }
+  }
 }
